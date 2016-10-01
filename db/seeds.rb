9272ce30fb4ea83d46b42dd85d8b06dc8eb9fd50
Nathan W <nate01776@gmail.com>
2016-10-01 13:40:10 -0400

    seeded player database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,18 @@ require 'json'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-player_data = File.read("./public/NFLjson/nflgame/players.json")
-sorted_player_data = JSON.parse(player_data)
 
-sorted_player_data.each do |player_key, player_data|
-  Player.create(
-    gsis: player_data[:gsis_id],
-    first_name: player_data[:first_name],
-    last_name: player_data[:last_name],
-    birthdate: player_data[:birthdate],
-    years_pro: player_data[:years_pro],
-    college: player_data[:college]
-  )
-  puts player_key
-end
+# PLAYER SEED DATA
+# player_data = File.read("./public/NFLjson/nflgame/players.json")
+# sorted_player_data = JSON.parse(player_data)
+#
+# sorted_player_data.each do |player_key, player_data|
+#   Player.create(
+#     gsis: player_data["gsis_id"],
+#     first_name: player_data["first_name"],
+#     last_name: player_data["last_name"],
+#     birthdate: player_data["birthdate"],
+#     years_pro: player_data["years_pro"],
+#     college: player_data["college"]
+#   )
+# end
