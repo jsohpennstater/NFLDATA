068a2f2fbab5349ac71ec77969bf069948bd0576
Nathan W <nate01776@gmail.com>
2016-10-04 18:20:52 -0400

    edited migration

--- db/migrate/20161004221655_added_game_columns.rb
+++ db/migrate/20161004221655_added_game_columns.rb
@@ -8,8 +8,8 @@ class AddedGameColumns < ActiveRecord::Migration[5.0]
 
   def down
     remove_column :games, :season_type, :string, null: false
-    remove_column :games, :season_type, :string, null: false
+    remove_column :games, :time, :string, null: false
     remove_column :games, :wday, :string, null: false
     remove_column :games, :week, :integer, null: false
-  endd
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004211901) do
+ActiveRecord::Schema.define(version: 20161004221655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 20161004211901) do
     t.integer  "season_id",      null: false
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "season_type",    null: false
+    t.string   "time",           null: false
+    t.string   "wday",           null: false
+    t.integer  "week",           null: false
     t.index ["season_id"], name: "index_games_on_season_id", using: :btree
   end
 
