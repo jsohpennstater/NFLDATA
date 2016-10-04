3b01bedade9e47b6f0105017359fa1215bc940f6
Nathan W <nate01776@gmail.com>
2016-10-04 17:20:22 -0400

    added matchup_number to stats

---
+++ db/migrate/20161004211901_add_matchup_number_column.rb
@@ -0,0 +1,9 @@
+class AddMatchupNumberColumn < ActiveRecord::Migration[5.0]
+  def up
+    add_column :stats, :matchup_number, :string, null:false
+  end
+
+  def down
+    remove_column :stats, :matchup_number, :string, null:false
+  end
+end
