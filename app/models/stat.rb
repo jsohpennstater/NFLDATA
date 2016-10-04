ddcb813f4f1de4ab7c8a103ef6f1ea2b63e48ba2
Nathan W <nate01776@gmail.com>
2016-10-04 18:04:49 -0400

    seeded stats, ready to seed playerstats join table

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,5 +1,5 @@
 class Stat < ActiveRecord::Base
-  # belongs_to :player
+  belongs_to :player
   # belongs_to :game
   # has_many :games, :through => :gamestats
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,66 +3,63 @@ require 'pry'
 
 
 # PLAYERSTATS SEED LOGIC
-# all_stats = Stat.all
-#
-# all_stats.each do |single_stat|
-#   player_object = Player.find_by(gsis: single_stat.gsis)
-#   Playerstat.create(player: player_object, stat: single_stat)
-# end
+all_stats = Stat.all
+
+all_stats.each do |single_stat|
+  player_object = Player.find_by(gsis: single_stat.gsis)
+  Playerstat.create(player: player_object, stat: single_stat)
+end
 
 
 # STAT TABLE SEED LOGIC
-Dir.foreach("./public/NFLjson/nflgame/gamecenter-json") do |file|
-  next if file == "." or file == ".."
-
-  game_data = File.read("./public/NFLjson/nflgame/gamecenter-json/" + file)
-  sorted_game_data = JSON.parse(game_data)
-
-  game_id = sorted_game_data.first.first
-
-  sorted_game_data[game_id]["home"]["stats"].each do |stat_type, type_data|
-    if stat_type == "passing" || stat_type == "rushing" || stat_type == "receiving"
-      type_data.each do |gsis, stat_data|
-        if gsis.length == 10
-          binding.pry
-          Stat.create(
-            stat_type: stat_type,
-            att: stat_data["att"],
-            cmp: stat_data["cmp"],
-            yds: stat_data["yds"],
-            tds: stat_data["tds"],
-            gsis: gsis,
-            lng: stat_data["lng"],
-            turn_overs: stat_data["ints"],
-            matchup_number: game_id
-          )
-        end
-      end
-    end
-  end
-
-  sorted_game_data[game_id]["away"]["stats"].each do |stat_type, type_data|
-    if stat_type == "passing" || stat_type == "rushing" || stat_type == "receiving"
-      type_data.each do |gsis, stat_data|
-        if gsis.length == 10
-          Stat.create(
-            stat_type: stat_type,
-            att: stat_data["att"],
-            cmp: stat_data["cmp"],
-            yds: stat_data["yds"],
-            tds: stat_data["tds"],
-            gsis: gsis,
-            lng: stat_data["lng"],
-            turn_overs: stat_data["ints"],
-            matchup_number: game_id
-          )
-        end
-      end
-    end
-  end
-  
-  puts "*"
-end
+# Dir.foreach("./public/NFLjson/nflgame/gamecenter-json") do |file|
+#   next if file == "." or file == ".."
+#
+#   game_data = File.read("./public/NFLjson/nflgame/gamecenter-json/" + file)
+#   sorted_game_data = JSON.parse(game_data)
+#
+#   game_id = sorted_game_data.first.first
+#
+#   sorted_game_data[game_id]["home"]["stats"].each do |stat_type, type_data|
+#     if stat_type == "passing" || stat_type == "rushing" || stat_type == "receiving"
+#       type_data.each do |gsis, stat_data|
+#         if gsis.length == 10
+#           Stat.create(
+#             stat_type: stat_type,
+#             att: stat_data["att"],
+#             cmp: stat_data["cmp"],
+#             yds: stat_data["yds"],
+#             tds: stat_data["tds"],
+#             gsis: gsis,
+#             lng: stat_data["lng"],
+#             turn_overs: stat_data["ints"],
+#             matchup_number: game_id
+#           )
+#         end
+#       end
+#     end
+#   end
+#
+#   sorted_game_data[game_id]["away"]["stats"].each do |stat_type, type_data|
+#     if stat_type == "passing" || stat_type == "rushing" || stat_type == "receiving"
+#       type_data.each do |gsis, stat_data|
+#         if gsis.length == 10
+#           Stat.create(
+#             stat_type: stat_type,
+#             att: stat_data["att"],
+#             cmp: stat_data["cmp"],
+#             yds: stat_data["yds"],
+#             tds: stat_data["tds"],
+#             gsis: gsis,
+#             lng: stat_data["lng"],
+#             turn_overs: stat_data["ints"],
+#             matchup_number: game_id
+#           )
+#         end
+#       end
+#     end
+#   end
+# end
 
 
 
